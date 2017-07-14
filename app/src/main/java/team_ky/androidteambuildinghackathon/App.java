b9949d81bf877045e079f46f124013ea0ad73346
Fuji Kinaga <kinaga@nana-music.com>
2017-07-14 22:16:15 +0900

    動いたぁぁぁあああ

--- app/src/main/java/team_ky/androidteambuildinghackathon/App.java
+++ app/src/main/java/team_ky/androidteambuildinghackathon/App.java
@@ -2,6 +2,9 @@ package team_ky.androidteambuildinghackathon;
 
 import android.app.Application;
 
+import cafe.adriel.androidaudioconverter.AndroidAudioConverter;
+import cafe.adriel.androidaudioconverter.callback.ILoadCallback;
+
 /**
  * Created by fujikinaga on 2017/07/14.
  */
@@ -12,5 +15,15 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        AndroidAudioConverter.load(this, new ILoadCallback() {
+            @Override
+            public void onSuccess() {
+                // Great!
+            }
+            @Override
+            public void onFailure(Exception error) {
+                // FFmpeg is not supported by device
+            }
+        });
     }
 }
