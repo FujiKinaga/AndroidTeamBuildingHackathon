573bea2dfd9628652bfeb7a0ac520f5fa768dd9c
akihito-okada <okada@nana-music.com>
2017-07-14 17:18:00 +0900

    change mainactivity

--- app/src/main/java/team_ky/androidteambuildinghackathon/MainActivity.java
+++ app/src/main/java/team_ky/androidteambuildinghackathon/MainActivity.java
@@ -1,17 +1,45 @@
 package team_ky.androidteambuildinghackathon;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @OnClick(R.id.download_sound_button)
+    void onClickDownloadSound(View view) {
+
+    }
+
+    @OnClick(R.id.ok_button)
+    void onClickOk(View view) {
+
+    }
+
+    @OnClick(R.id.share_instagram)
+    void onShareInstagram(View view) {
+        Intent intent = new Intent(this, ShareToInstagramActivity.class);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, ShareToInstagramActivity.class);
-        startActivity(intent);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(R.string.lbl_download_sound);
     }
 }
